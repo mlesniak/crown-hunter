cc851cb0834cf7aa9977fcf36ce32b1228671714
Michael Lesniak <mail@mlesniak.com>
2021-04-02 14:43:14 +0200

    Initial commit

---
+++ src/main/kotlin/GameDemo/GameDemo.kt
@@ -0,0 +1,23 @@
+package GameDemo
+
+import com.badlogic.gdx.ApplicationListener
+
+class GameDemo : ApplicationListener {
+    override fun create() {
+    }
+
+    override fun resize(p0: Int, p1: Int) {
+    }
+
+    override fun render() {
+    }
+
+    override fun pause() {
+    }
+
+    override fun resume() {
+    }
+
+    override fun dispose() {
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+import GameDemo.GameDemo
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+
+fun main() {
+    val config = LwjglApplicationConfiguration()
+    config.title = "Drop"
+    config.width = 800
+    config.height = 600
+    LwjglApplication(GameDemo(), config)
+}
